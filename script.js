75ecccf95943e317d84b603a3ec26d20af1fa32d
Mark Abramov <markabramov01@gmail.com>
2019-05-12 20:59:06 -0400

    make a basic transition

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
 $(function () {
-    $("#slider").slider({
+    $slider = $("#slider");
+    $slider.slider({
         value: 2007,
         min: 2007,
         max: 2018,
         step: 1,
         slide: function (event, ui) {
             $("#amount").val(ui.value);
-            let map = document.getElementsByClassName("map-container")[0];
-            map.parentNode.removeChild(map);
             makeMap(ui.value);
+            setTimeout(function(){
+                let map = document.getElementsByClassName("map-container")[0];
+                map.parentNode.removeChild(map);
+            },1000);
         }
     });
-    let year = $("#slider").slider("value");
+    let year = $slider.slider("value");
     $("#amount").val(year);
-
     makeMap(year);
 });
 
 function makeMap(year) {
-    
-    var format = d3.format(",");
+    let format = d3.format(",");
 
 // Set tooltips
-    var tip = d3.tip()
+    let tip = d3.tip()
         .attr('class', 'd3-tip')
         .offset([-10, 0])
         .html(function (d) {
             return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Students: </strong><span class='details'>" + format(isNaN(d["students" + year]) ? 0 : d["students" + year]) + "</span>";
         });
 
-    var margin = {top: 0, right: 0, bottom: 0, left: 0},
+    let margin = {top: 0, right: 0, bottom: 0, left: 0},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-    var color = d3.scaleThreshold()
+    let color = d3.scaleThreshold()
         .domain([0, 1, 5, 10, 25, 50, 100, 250, 500, 1000])
         .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)", "rgb(33,113,181)", "rgb(8,81,156)", "rgb(8,48,107)", "rgb(3,19,43)"]);
 
-    var path = d3.geoPath();
-
-    var svg = d3.select("body")
+    let svg = d3.select("body")
         .append("svg")
         .attr("width", width)
         .attr("height", height)
         .append('g')
         .attr('class', 'map');
 
-    var projection = d3.geoMercator()
+    let projection = d3.geoMercator()
         .scale(130)
         .translate([width / 2, height / 1.5]);
 
-    var path = d3.geoPath().projection(projection);
+    let path = d3.geoPath().projection(projection);
 
     svg.call(tip);
 
@@ -61,7 +60,7 @@ function makeMap(year) {
         .await(ready);
 
     function ready(error, data, enrollmentYear) {
-        var studentsByCountryId = {};
+        let studentsByCountryId = {};
         console.log("data", data, "enrollmentYear", enrollmentYear, "studentsByCountryId", studentsByCountryId);
         enrollmentYear.forEach(function (d) {
             studentsByCountryId[d.id] = +d["students" + year];
